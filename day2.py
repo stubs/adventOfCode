51e5d67f64f6dd3c9be85309ca2a1a89f076c370
Aaron Gonzalez <a.gonzalez979@gmail.com>
2015-12-27 15:37:19 -0500

    Add solution for day 2 puzzle 2.

--- day2.py
+++ day2.py
@@ -1,7 +1,8 @@
-#------PROBLEM 1 --------
+#------Day 2 --------
 filename = 'day2.txt'
 
 cum_total_area = 0
+ribbon_total = 0
 
 def surfacearea(length, width, height):
     leng_area = length * width
@@ -9,12 +10,26 @@ def surfacearea(length, width, height):
     h_area = length * height
 
     area = 2*leng_area + 2*wid_area + 2*h_area + min(leng_area, wid_area, h_area)
-    return area 
+
+    if min(leng_area, wid_area, h_area) == leng_area:
+        ribbon = (2*length) + (2*width)
+    elif min(leng_area, wid_area, h_area) == wid_area:
+        ribbon = (2*width) + (2*height)
+    else:
+        ribbon = (2*length) + (2*height)
+
+    ribbon += (length * width * height)
+
+    return area , ribbon
+
 
 with open(filename, 'r') as F:
     for i in F:
         l, w, h = i.split('x')
-        cum_total_area += surfacearea(int(l),int(w),int(h))
+        area, ribbon = surfacearea(int(l), int(w), int(h))
+        cum_total_area += area 
+        ribbon_total += ribbon
 
 print "Elves will need " + str(cum_total_area) + " square feet of wrapping paper."        
+print "They are also going to need " + str(ribbon_total) + " feet of ribbon."
 # ------------------------------------
