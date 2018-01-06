f06352dfd43dbb77139f2e35f814219b45fdc59b
Aaron Gonzalez <a.gonzalez979@gmail.com>
2018-01-06 13:53:29 -0500

    clean-up

--- seventeen/day4/day4.py
+++ seventeen/day4/day4.py
@@ -6,11 +6,7 @@ def part1(in_file):
     regex_pattern = re.compile(r'(\b\w+\b\s?).*\1')
     fin = open(in_file, 'r').readlines()
     total_lines = len(fin)
-
-    for line in fin:
-        if re.search(regex_pattern, line):
-            total_lines -= 1
-
-    return total_lines
+    dups = [1 for line in fin if re.search(regex_pattern, line)]
+    return total_lines - sum(dups)
 
 #def part2():
